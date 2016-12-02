26e732aa45b15a0662cef671e2ed389f55e9d361
潘俊俊 <keyps2@icloud.com>
2016-12-02 18:27:27 +0800

    update reading file example

--- john_data_learning.py
+++ john_data_learning.py
@@ -5,13 +5,14 @@
 """
 from os.path import join, splitext
 from meta_data import header
-# import pandas
+import pandas
 import os
 
 
-bank_detail_file_name = "bank_detail_train.txt"
-bank_detail_file = join(join(os.getcwd(), "train"), bank_detail_file_name)
-# bank_detail_df = pandas.read_table(bank_detail_file, sep=",")
-# bank_detail_df.columns = header.get(splitext(bank_detail_file_name)[0])
+bank_detail_file_name = "bank_detail_test.txt"
+dir_name = splitext(bank_detail_file_name)[0].split("_")[-1]
+bank_detail_file = join(join(os.getcwd(), dir_name), bank_detail_file_name)
+bank_detail_df = pandas.read_table(bank_detail_file, sep=",")
+bank_detail_df.columns = header.get(splitext(bank_detail_file_name)[0])
 print header.get(splitext(bank_detail_file_name)[0])
-
+print bank_detail_df.head(3)
