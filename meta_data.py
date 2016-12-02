840ca467ffd66426b0290e2f38ea07d9f97f0593
潘俊俊 <keyps2@icloud.com>
2016-12-02 18:27:38 +0800

    update meta data

--- meta_data.py
+++ meta_data.py
@@ -12,13 +12,19 @@ from __future__ import unicode_literals
 header = dict(
     bank_detail_train=["用户id", "时间戳", "交易类型", "交易金额", "工资收入标记"],
     bank_detail_test=["用户id", "时间戳", "交易类型", "交易金额", "工资收入标记"],
-    bill_detail_train="",
-    bill_detail_test="",
-    browse_history_train="",
-    browse_history_test="",
-    loan_time_train="",
-    loan_time_test="",
-    overdue_train="",
-    user_info_train="",
-    usersID_test="",
+    bill_detail_train=[
+        "用户id", "账单时间戳", "银行id", "上期账单金额", "上期还款金额", "信用卡额度", "本期账单余额", "本期账单最低还款额",
+        "消费笔数", "本期账单金额", "调整金额", "循环利息", "可用金额", "预借现金额度", "还款状态"
+    ],
+    bill_detail_test=[
+        "用户id", "账单时间戳", "银行id", "上期账单金额", "上期还款金额", "信用卡额度", "本期账单余额", "本期账单最低还款额",
+        "消费笔数", "本期账单金额", "调整金额", "循环利息", "可用金额", "预借现金额度", "还款状态"
+    ],
+    browse_history_train=["用户id", "时间戳", "浏览行为数据", "浏览子行为编号"],
+    browse_history_test=["用户id", "时间戳", "浏览行为数据", "浏览子行为编号"],
+    loan_time_train=["用户id", "放款时间"],
+    loan_time_test=["用户id", "放款时间"],
+    overdue_train=["用户id", "样本标签"],
+    user_info_train=["用户id", "性别", "职业", "教育程度", "婚姻状态", "户口类型"],
+    usersID_test=["用户id"],
 )
