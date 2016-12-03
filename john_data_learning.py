d482f839445de31b7454a8389a215030309c0e14
潘俊俊 <keyps2@icloud.com>
2016-12-04 02:29:48 +0800

    更新测试

--- john_data_learning.py
+++ john_data_learning.py
@@ -4,17 +4,8 @@
 这里用来浏览数据集, 根据需要可以将这些代码运行在jupyter notebook上
 """
 from helpers import read_data_set
-from meta_data import tables
-import traceback
+from meta_data import TABLES
 
 
-for table in tables:
-    try:
-        print read_data_set(table).head(3)
-    except:
-        print "\n" + "-" * 100
-        print table
-        traceback.print_exc()
-        print "-" * 100 + "\n"
-        continue
-
+for table in TABLES:
+    print read_data_set(table).describe()
