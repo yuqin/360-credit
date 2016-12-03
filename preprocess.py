28acf8b676e73c4b87f29b827d99832ac79a2668
潘俊俊 <keyps2@icloud.com>
2016-12-04 02:28:57 +0800

    增加银行详细流水的etl

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@
 数据清洗根据不同的数据源有多种pipeline (class为单位), 每个pipeline都有好几步 (function为单位),
 """
 from abc import abstractmethod
+from helpers import *
 
 
 class BaseETL(object):
@@ -14,7 +15,10 @@ class BaseETL(object):
     :return:
     """
     def __init__(self):
+        self.file_name = ""
         self.data = None
+        self.cleaned = False
+        return
 
     @abstractmethod
     def extract(self):
@@ -40,7 +44,38 @@ class BaseETL(object):
         """
         pass
 
-    def etl(self):
+    def etl(self, file_name):
+        self.file_name = file_name
         self.extract()
         self.transform()
         self.load()
+        return
+
+
+class BankDetailETL(BaseETL):
+    """
+    清洗bank_detail_train.txt和bank_detail_test.txt文件
+    """
+    def __init__(self):
+        BaseETL.__init__(self)
+        return
+
+    def extract(self):
+        self.data = read_data_set(self.file_name, self.cleaned)
+        return
+
+    def transform(self):
+        """
+        1. <时间戳>转换
+        2. <交易类型>转换
+        3. <工资收入标记>转换
+        :return:
+        """
+        self.data[u"时间戳"] = self.data[u"时间戳"].apply(unixtime_to_datetime)
+        self.data[u"交易类型"] = self.data[u"交易类型"].apply(get_column_meaning(u"交易类型"))
+        self.data[u"工资收入标记"] = self.data[u"工资收入标记"].apply(get_column_meaning(u"工资收入标记"))
+        return
+
+    def load(self):
+        save_data_set(self.data, self.file_name)
+        return
