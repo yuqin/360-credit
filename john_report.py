e089c70b7e30ca57bd5929112e5db5691bc3817d
潘俊俊 <keyps2@icloud.com>
2016-12-04 02:28:07 +0800

    测试etl

--- john_report.py
+++ john_report.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 # __author__: u"John"
-# from helpers import *
-# from preprocess import *
+from helpers import *
+from preprocess import *
 # from model_research import *
 
+file_name = "bank_detail_train.txt"
+BankDetailETL().etl(file_name)
+print read_data_set(file_name, True).head()
